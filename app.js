b0b98a70ac0239c72a29d9cbd1448d3acaa2bad0
Keith Lockey <lockey.keith@gmail.comm>
2020-12-27 10:53:19 -0500

    changes

--- app.js
+++ app.js
@@ -12,79 +12,126 @@
         this.fact = fact;
     }
 
-
-    // Create Dino Objects
+// Create Dino Objects
    let buttonClick = document.getElementById('btn');
+
    buttonClick.onclick = function(){
-
-       let dinos= [];
-
+        let dinos= [];
         const getDinoData = async () => {
         const fetchedData = await fetch("./dino.json");
         const data = await fetchedData.json();
         //console.log(data);
         return data;
       };
-     
+
       
-
-
  // Create Human Object
-
- 
- function Human(nameInput,feetInput,inchesInput,weightInput,dietSelect){
-  this.name = nameInput;
-  this.feet = feetInput; 
-  this.inches = inchesInput;
-  this.weightInput = weightInput;
-  this.diet = dietSelect;
+ function Human(name,feet,inches,weight,diet){
+  this.name = name;
+  this.height = (12 * feet)  + inches;  
+  this.weight = weight;
+  this.diet = diet;
  }
 
-let human  = (function () {
- let nameInput = document.getElementById('name').value;
- let feetInput = document.getElementById('feet').value;
- let inchesInput = document.getElementById('inches').value;
- let weightInput =  document.getElementById('weight').value;
- let dietSelect = document.getElementById('diet').value; 
-
-return new Human(nameInput,feetInput,inchesInput,weightInput,dietSelect);
-
+ let human  = (function () {
+ let nameInput = document.getElementById("name").value;
+ let feetInput = document.getElementById("feet").value;
+ let inchesInput = document.getElementById("inches").value;
+ let weightInput =  document.getElementById("weight").value;
+ let dietSelect = document.getElementById("diet").value; 
+ return new Human(nameInput,feetInput,inchesInput,weightInput,dietSelect);
 })();
 
-
 const createDinos = async () => {
-  dinos = await getDinoData(); // will return dino array
-// return dinos;
+dinos = await getDinoData(); // will return dino array
 
-herbavor = []; 
-omnivoir = [];
-carnivor = [];
+
+// return dinos;
+let herbavor = []; 
+let omnivor = [];
+let carnivor = [];
+let herbavorObject = {name: "", type: "", weight: "", height:""};
+let omnivorObject = {name: "", type: "", weight: "", height:""};
+let carnivorObject = {name: "", type: "", weight: "", height:""};
+
+
 
 dinos.Dinos.forEach(dino => {
-//   console.log(dino.species);
-if (dino.diet == 'herbavor'){
-    herbavor.push(dino);
-}else if(dino.diet == 'omnivor'){
-    omnivor.push(dino);
-}else if(dino.diet == 'carnivor'){
-    carnivor.push(dino);
+if (dino.diet == "herbavor"){
+  herbavorObject.name = dino.name;
+  herbavorObject.type = dino.species;
+  herbavorObject.weight = compareWeight(dino.weight);
+  herbavorObject.height = compareHeight(dino.height);
+  herbavor.push(herbavorObject);
+}else if(dino.diet == "omnivor"){
+  omnivorObject.name = dino.name;
+  omnivorObject.type = dino.species;
+  omnivorObject.weight = compareWeight(dino.weight);
+  omnivorObject.height = compareHeight(dino.height);
+  omnivor.push(dino);
+}else if(dino.diet == "carnivor"){
+  carnivorObject.name = dino.name;
+  carnivorObject.type = dino.species;
+  carnivorObject.weight = compareWeight(dino.weight);
+  carnivorObject.height = compareHeight(dino.height);
+  carnivor.push(dino);
 }
 });
 
+console.log(carnivorObject);
+
+
+function compareWeight(weight){
+ // console.log(weight);
+  if (human.weight > weight)
+     return "Human weight is greater than ";
+  else if (human.weight < weight)
+     return "Human weight is less than ";
+  else 
+     return "Human weight is equal to ";
+}
+
+function compareHeight(height) {
+  if (human.height > height)
+     return "Human height is greater than ";
+  else if (human.height < height)
+     return "Human height is less than ";
+  else 
+     return "Human Height is equal";
+}
+
+document.getElementById('dino-compare').style.visibility = "hidden"; 
+
+//console.log(dinos.Dinos);
+
+(function () {
+    dinos.Dinos.forEach(dino => {
+    const grid = document.querySelector("#grid");
+    grid.innerHTML += `
+    <div class="grid-item">
+        <h3> ${dino.species} </h3>
+        <img src="./"${changeLowerCase(dino.species)}.png" alt="" />
+        <p>facts</p>    
+    </div>
+  `;
+})
+})();
+
+function changeLowerCase(word) {
+  return word.toLowerCase();
+}
 
 console.log(herbavor);
 }
 
-//Math.floor(Math.random() * arr.length)
-
-
 createDinos();
 
-//console.log(human);
-
-// document.getElementById('dino-compare').style.visibility = "hidden"; 
-
 }
+
+
+    //  Math.floor(Math.random() * arr.length)
+     //console.log(human);
+ 
 
     // let dino = new dinosaur(1,2,3,4,5,6,7);
     // console.log(dino);

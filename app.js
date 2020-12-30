f03e5cb7d318be44c2052979b00a7825d52bd8d0
Keith Lockey <lockey.keith@gmail.comm>
2020-12-30 17:53:12 -0500

    comparisons

--- app.js
+++ app.js
@@ -20,73 +20,77 @@
         const getDinoData = async () => {
         const fetchedData = await fetch("./dino.json");
         const data = await fetchedData.json();
-        //console.log(data);
- //       console.log("hello");
         return data;
       };
 
       
  // Create Human Object
- function Human(name,feet,inches,weight,diet){
-  this.name = name;
+ function Human(species,feet,inches,weight,diet){
+  this.species = species;
   this.height = (12 * feet)  + inches;  
   this.weight = weight;
   this.diet = diet;
  }
 
- let human  = (function () {
- let nameInput = document.getElementById("name").value;
- let feetInput = document.getElementById("feet").value;
- let inchesInput = document.getElementById("inches").value;
- let weightInput =  document.getElementById("weight").value;
- let dietSelect = document.getElementById("diet").value; 
- return new Human(nameInput,feetInput,inchesInput,weightInput,dietSelect);
-})();
+//  let human  = (function () {
+//  let nameInput = document.getElementById("name").value;
+//  let feetInput = document.getElementById("feet").value;
+//  let inchesInput = document.getElementById("inches").value;
+//  let weightInput =  document.getElementById("weight").value;
+//  let dietSelect = document.getElementById("diet").value; 
+//  return new Human(nameInput,feetInput,inchesInput,weightInput,dietSelect);
+// })();
 
 const createDinos = async () => {
 dinos = await getDinoData(); // will return dino array
 
+let dinosaurs = [];
 
 // return dinos;
 let dinosaur = []; 
-
 let dinosaurObject = {species: "", diet: "", weight: "", height:""};
-
 let diet = document.getElementById('diet').value;
 
-let count = 0;
-
 dinos.Dinos.forEach(dino => {
+  //console.log(dino.species);
   if (diet.toLowerCase() == dino.diet){
     dinosaurObject.species = dino.species;
-    dinosaur[count] = dinosaurObject;
-    count++;
+    dinosaurObject.diet = dino.diet;
+    dinosaurObject.weight = dino.weight;
+    dinosaurObject.height = dino.height;
+    dinosaur.push(Object.assign({}, dinosaurObject)); 
   }
-  
-  console.log(dinosaur);
+});
 
+let cnt =0;
+
+let human = new Human("Human","","","","");
+
+dinos.Dinos.forEach(dino => {
+if (cnt == 4){
+  dinosaurs.push(human); 
+}
+dinosaurs.push(dino);
+cnt++;
 });
 
 
-dinosaur.forEach(a => {
-console.log("dinosaur object " + a.name);
-});
-
+// console.log(dinosaur);
 
 function compareWeight(weight){
   if (human.weight > weight)
-     return "Human weight is greater than ";
+     return "Human weight is greater";
   else if (human.weight < weight)
-     return "Human weight is less than ";
+     return "Human weight is less";
   else 
      return "Human weight is equal to ";
 }
 
 function compareHeight(height) {
   if (human.height > height)
-     return "Human height is greater than ";
+     return "Human height is greater";
   else if (human.height < height)
-     return "Human height is less than ";
+     return "Human height is less";
   else 
      return "Human Height is equal";
 }
@@ -97,27 +101,89 @@ function compareDiet(diet) {
   return "Human diet is different";
 }
 
-
 document.getElementById('dino-compare').style.visibility = "hidden"; 
 
+let count = 0;
+let filler;
+const grid = document.querySelector("#grid");
+
+const getRandomFact = () => {
+  const factArray = [
+    'where',
+    'when',
+    'fact',
+    'compareHeight',
+    'compareDiet',
+    'compareWeight',
+  ];
+  const randomIndex = Math.floor(Math.random() * factArray.length);
+  const fact = factArray[randomIndex];
+  return fact;
+};
+
+function getKeyByValue(object, value) { 
+  for (var prop in object) { 
+      if (object.hasOwnProperty(prop)) { 
+          if (object[prop] === value) 
+          return prop; 
+      } 
+  } 
+} 
+
+//let randomfact =  getRandomFact();
+let fact="";
 
 (function () {
-    dinos.Dinos.forEach(dino => {
-    const grid = document.querySelector("#grid");
+    dinosaurs.forEach(dino => {   
+      
+    let randomfact = getRandomFact();
+
+    if (randomfact == "compareWeight" ){
+      fact = compareWeight(dino.weight);
+    } else if (randomfact == "compareHeight"){
+      fact = compareHeight(dino.height);
+    } else if (randomfact == "compareDiet"){
+      fact = compareDiet(dino.diet);
+    } else {
+      fact = dino[randomfact];
+    }
+
+  
+    console.log(fact);
+
+    if (dino.species == 'Human'){
+      grid.innerHTML += `
+      <div class="grid-item">
+          <h3> ${dino.species} </h3>
+          <img src="images/${changeLowerCase(dino.species)+'.png'}" alt="" />
+          <div> </div> 
+          <div> </div>
+       </div>`;
+    } else if (dino.species == "Pigeon") {
     grid.innerHTML += `
     <div class="grid-item">
         <h3> ${dino.species} </h3>
-        <img src="./"${changeLowerCase(dino.species)}.png" alt="" />
-        <p>facts</p>    
-    </div>
-  `;
-})
+        <img src="images/${changeLowerCase(dino.species)+'.png'}" alt="" />
+        <div  style="text-align:center; width:100%; white-space: nowrap; overflow: hidden;"> ${dino.fact} </div> 
+        <div></div>
+     </div>`;
+    } else {
+      grid.innerHTML += `
+      <div class="grid-item">
+          <h3> ${dino.species} </h3>
+          <img src="images/${changeLowerCase(dino.species)+'.png'}" alt="" />
+          <div id="fact" style="text-align:center; width:100%; white-space: nowrap; overflow: hidden;">${fact} </div> 
+         <!-- <div  style="text-align:center; width:100%; white-space: nowrap; overflow: hidden;"> Weight:  ${dino.weight} </div> -->
+       </div>`;
+   }
+;})
 })();
 
+
 function changeLowerCase(word) {
+ // console.log(word);
   return word.toLowerCase();
 }
-
 
 }
 
